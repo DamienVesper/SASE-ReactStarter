5eda3f22bbf16c9b80255751eafb7bbe8a1551c1
Damien Vesper <ldamienvesper@gmail.com>
2023-09-18 20:29:37 -0500

    files

---
+++ geauxhack.js
@@ -0,0 +1,53 @@
+// x = 5;
+// y = "Hello";
+
+// let s = 10;
+// var z = 10;
+
+// // THE SECOND PART IS A LIE IT IS STILL SUPPORTED OTHERWISE BROWSERS THEMSELVES WOILD BREAK BECAUSE THEY STILL USE VAR
+// // ANY WEBSITE USING WEBPACK WOULD BREAK BECAUSE THEY USE VAR\
+
+// // const c = 3.14;
+// // c = 10;
+
+// // console.log(c);
+
+// {
+//     let x = 10;
+//     var v = 100;
+// }
+
+// console.log(v);
+
+// // number
+// let age = 25;
+// let price = 19.99;
+
+// // string
+// let name = `Alice`;
+// let greeting = `Hello, World`;
+
+// // boolean
+// let isRaining = true;
+//  let isSunny = false;
+
+// //  Array
+// let colors = [];
+
+
+// a = 10;
+// b = 5;
+
+// let isEqual = a == b; // equality check
+// let isGreaterThan = a > b;
+
+// function greet(name) {
+//     // 5 + 6
+//     return "hello, " + name + "!";
+// }
+
+// const doMath = (x, y) => x * y - 10;
+
+const add = (a, b) => a + b;
+console.log(add(3, 5));
+
